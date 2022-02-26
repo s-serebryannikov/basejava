5faa52c246ecdf07abccb7568ab96d933e1d1f1a
Sergey Serebryannikov <s.serebryannikovvv@mail.ru>
2022-02-26 16:46:34 +0300

    add method update and correction method save, get, delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,44 +4,61 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[6];
     private int size;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+        int count = 0;
+        if (size != storage.length) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid().equals(r.getUuid())) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                storage[size] = r;
+                size++;
+            } else System.out.println("Резюме с таким uuid уже существует");
+        } else System.out.println("Архив резюме переполнен");
+    }
+
+    public void update(Resume r) {
         int count = 0;
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(r.getUuid())) {
+                storage[i] = r;
+                System.out.println("Резюме " + r.getUuid() + " удачно перезаписано");
                 count++;
+                break;
             }
         }
-        if (count == 0) {
-            storage[size] = r;
-            size++;
-        } else System.out.println("Резюме с таким uuid уже существует.");
+        if (count == 0) System.out.println("Резюме с " + r.getUuid() + " не существует.");
     }
 
     public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return storage[i];
-            }
+            } else System.out.println("Резюме с " + uuid + " не существует");
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
-                break;
+            } else {
+                System.out.println("Резюме c " + uuid + " отсутствует!");
             }
+            break;
         }
     }
 
